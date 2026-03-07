b2271250591455522c57ad5b3509f3aa47779793
Saleh <you@example.com>
2026-03-07 16:24:21 +0400

    Add database reset script and reset user data

---
+++ script/reset-db.ts
@@ -0,0 +1,27 @@
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "../shared/schema";
+
+console.log("Resetting database...");
+
+const sqliteDb = new Database("./eltizam.db");
+
+// Drop existing tables
+sqliteDb.exec(`
+  DROP TABLE IF EXISTS transactions;
+  DROP TABLE IF EXISTS wallets;
+  DROP TABLE IF EXISTS categories;
+  DROP TABLE IF EXISTS users;
+`);
+
+console.log("Tables dropped");
+
+const db = drizzle(sqliteDb, { schema });
+
+// Create tables by running the schema
+const { users, wallets, categories, transactions } = schema;
+
+console.log("Database reset complete!");
+console.log("You can now register a new account.");
+
+sqliteDb.close();
